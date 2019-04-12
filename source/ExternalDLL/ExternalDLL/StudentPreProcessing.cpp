96cf054fee4909997c44cfbda5797f117ccf00e8
Joost van Lingen <joost96nl@gmail.com>
2019-04-12 17:19:21 +0200

    Changed RGB Intensity conversion to Luster formula

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,14 +1,18 @@
 #include "StudentPreProcessing.h"
+#include <algorithm>
 
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
 	IntensityImageStudent * intensity_image = new IntensityImageStudent(image.getWidth(), image.getHeight());
 	for (int i = 0; i < image.getWidth() * image.getHeight(); i++) {
-		unsigned char r, g, b;
-		r = image.getPixel(i).r * 0.3;
-		g = image.getPixel(i).g * 0.59;
-		b = image.getPixel(i).b * 0.11;
-		intensity_image->setPixel(i, (r + g + b));
+		int r, g, b;
+		r = int(image.getPixel(i).r);
+		g = int(image.getPixel(i).g);
+		b = int(image.getPixel(i).b);
+		int max = std::max(std::max(r,g), std::max(g, b));
+		int min = std::max(std::max(r, g), std::max(g, b));
+		int c = 0.5 * ((uint64_t)max + (uint64_t)min);
+		intensity_image->setPixel(i, c);
 	}
 	return intensity_image;
 }
